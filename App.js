91caa225cffb23e6d1ad027b5e8d80f4093ac9b4
Denis Matveev <mast747@bk.ru>
2016-08-01 02:56:46 +0300

    Add slider component with ref

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      txt: 'this is the text',
-      cat: 0
-    }
+      red: 0,
+      green: 0,
+      blue: 0
+    };
+    this.update = this.update.bind(this);
   }
 
   update(e) {
-    this.setState({txt: e.target.value});
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red).value,
+      green: ReactDOM.findDOMNode(this.refs.green).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue).value
+    });
   }
 
   render() {
@@ -17,11 +25,17 @@ class App extends React.Component {
     let cat = this.props.cat;
     return (
       <div data-cat={cat}>
-        <input type="text"
-          onChange={this.update.bind(this)} />
         <h1>{txt}</h1>
-        <p>{this.state.txt}</p>
-      </div>);
+        <Slider ref="red" update={this.update} />
+        {this.state.red}
+        <br />
+        <Slider ref="green" update={this.update} />
+        {this.state.green}
+        <br />
+        <Slider ref="blue" update={this.update} />
+        {this.state.blue}
+      </div>
+    );
   }
 }
 
@@ -32,6 +46,17 @@ App.propTypes = {
 
 App.defaultProps = {
   cat: 1
+}
+
+class Slider extends React.Component {
+  render() {
+    return (
+        <input type="range"
+          min="0"
+          max="255"
+          onChange={this.props.update} />
+        );
+  }
 }
 
 export default App;
