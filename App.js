c841e0ce82f2fe8fa5ba8f46c56a73b7e55803db
Denis Matveev <mast747@bk.ru>
2016-08-01 01:10:51 +0300

    Add props to component

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 class App extends React.Component {
   render() {
-    return <div>Hello!!!</div>
+    let txt = this.props.txt;
+    let cat = this.props.cat;
+    return <div data-cat={cat}>{txt}</div>
   }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  cat: 1
 }
 
 export default App;
